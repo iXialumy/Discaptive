b3113e831daf03a7990a45dd6e5fec23356c2d77
iXialumy <pausjonas@gmail.com>
2016-05-18 01:32:26 +0200

    Error in readLine

--- src/GameController/GameContorller.java
+++ src/GameController/GameContorller.java
@@ -6,11 +6,11 @@ import In_Out.*;
 /**
  * Created by jpaus on 17.05.16.
  */
-public class GameContorller {
+class GameContorller {
     private char[][] board;
 
-    public GameContorller(int level) {
-        String filename = "res/Level" + level + ".txt";
+    GameContorller(int level) {
+        String filename = "file:res/Level" + level + ".txt";
         openBoard(filename);
     }
 
@@ -18,17 +18,36 @@ public class GameContorller {
     	ArrayList<String> lineArray = new ArrayList<String>();
         In.open(filename);
         String line;
-        for (int i = 0; (line = In.readLine()) != null ; i++) {
-    		lineArray.set(i, line);
+        for (int i = 0; true ; i++) {
+            line = In.readLine(); //TODO: fix
+            if (line == null)
+                break;
+            lineArray.set(i, line);
     	}
         int longestLine = 0;
-        for(String line: lineArray) {
-            int len = line.length();
-            if(len > longestLine)
+        for(String line2: lineArray) {
+            int len = line2.length();
+            if(len > longestLine) {
                 longestLine = len;
+            }
         }
-
+        for (int i = 0; i < lineArray.size(); i++) {
+            for (int j = 0; j < longestLine; j++) {
+                board[i][j] = lineArray.get(i).charAt(j);
+            }
+        }
     }
-
+    
+    String getBoardf() {
+        String output = "";
+        for (int i = 0, boardLength = board.length; i < boardLength; i++) {
+            char[] aBoard = board[i];
+            for (int j = 0; j < aBoard.length; j++) {
+                output += aBoard[j];
+            }
+            output += "\n";
+        }
+        return output;
+    }
 
 }
--- src/GameController/Starter.java
+++ src/GameController/Starter.java
@@ -1,9 +1,16 @@
 package GameController;
 
+import In_Out.Out;
+
 /**
  * Created by jpaus on 17.05.16.
  */
 public class Starter {
+    private static GameContorller gc;
 
+    public static void main(String[] args) {
+        gc = new GameContorller(1);
+        Out.println(gc.getBoardf());
+    }
 
 }
