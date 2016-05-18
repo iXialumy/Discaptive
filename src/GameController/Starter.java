efae19496e2749cf53112291b8fc14c62c8eca43
iXialumy <pausjonas@gmai.com>
2016-05-18 13:58:36 +0200

    File reader fixed

--- src/GameController/Starter.java
+++ src/GameController/Starter.java
@@ -6,10 +6,10 @@ import In_Out.Out;
  * Created by jpaus on 17.05.16.
  */
 public class Starter {
-    private static GameContorller gc;
+    private static GameController gc;
 
     public static void main(String[] args) {
-        gc = new GameContorller(1);
+        gc = new GameController(1);
         Out.println(gc.getBoardf());
     }
 
